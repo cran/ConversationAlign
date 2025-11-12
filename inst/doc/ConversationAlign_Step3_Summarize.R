79b89e9935b65978c6024075b9127194228b2ccf
Jamie Reilly <jamie_reilly@temple.edu>
2025-11-12 23:30:31 +0000

    version 0.4.0

--- inst/doc/ConversationAlign_Step3_Summarize.R
+++ inst/doc/ConversationAlign_Step3_Summarize.R
@@ -5,11 +5,18 @@ knitr::opts_chunk$set(
 )
 
 ## ----message=FALSE, warning=F, echo=F-----------------------------------------
-# Load SemanticDistance
+# Load ConversationAlign
+# library(devtools)
+# devtools::install_github("https://github.com/Reilly-ConceptsCognitionLab/ConversationAlign.git")
 library(ConversationAlign)
 
-## ----eval=T, warning=F, message=F, options(digits = 3)------------------------
+## ----eval=T, warning=F, message=F, options(digits=3)--------------------------
 MarySumDat <- summarize_dyads(df_prep = NurseryRhymes_Prepped, custom_lags=NULL, sumdat_only = TRUE, corr_type='Pearson') 
 colnames(MarySumDat)
 knitr::kable(head(MarySumDat, 15), format = "simple", digits = 3)
+
+## ----eval=T, warning=F, message=F---------------------------------------------
+MaryShams <- generate_shams(df_prep = NurseryRhymes_Prepped, seed = 10)
+MarySumDatShams <- summarize_dyads(df_prep = MaryShams, custom_lags=NULL, sumdat_only = TRUE, corr_type='Pearson') 
+knitr::kable(head(MarySumDatShams, 15), format = "simple", digits = 3)
 
