2a1534987aceea2bfa3d432e9169bd8ca0e6413b
Jamie Reilly <jamie_reilly@temple.edu>
2026-04-29 15:10:25 +0000

    version 0.4.1

--- inst/doc/ConversationAlign_Step1_Read.R
+++ inst/doc/ConversationAlign_Step1_Read.R
@@ -15,8 +15,8 @@ library(ConversationAlign)
 # #will scan custom folder called 'MyStuff' in your current directory, concatenating all files in that folder into a single dataframe
 # MyConvos2 <- read_dyads(my_path='/MyStuff')
 
-## ----eval=T, message=F, warning=F---------------------------------------------
-MaryLittleLamb <- read_1file(MaronGross_2013)
-#print first ten rows of header
-knitr::kable(head(MaronGross_2013, 15), format = "pipe")
+## ----eval=F, message=F, warning=F---------------------------------------------
+# MaryLittleLamb <- read_1file(MaronGross_2013)
+# #print first ten rows of header
+# knitr::kable(head(MaronGross_2013, 15), format = "pipe")
 
